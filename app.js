b66f8305408e39bf73a8e1d0bec51250f3668de1
saptarshi3112 <senguptasaptarshi11@gmail.com>
2019-10-31 19:32:02 +0530

    callRouter

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+const fs = require('fs');
+
+const Nexmo = require('nexmo');
 
 const app = express();
 
-app.get('/userRouter', (req, res) => {
-  res.json({
-    message: 'hey bubau'
-  });
+const nexmo = new Nexmo({
+  apiKey: '975d0eba',
+  apiSecret: 'G6LWgEJFnCGrgMkD',
+  applicationId: 'e86bceea-a49f-43cf-945e-df3944a6ceb8',
+  privateKey: fs.readFileSync('private.key')
+});
+
+const ncco = [
+  {
+    action: 'talk',
+    voiceName: 'Joey',
+    text:
+      'This is a text-to-speech test message.',
+  },
+];
+
+app.get('/userRouter/:number', (req, res) => {
+  const number = req.params.number;
+  nexmo.calls.create({
+      to: [{ type: 'phone', number: number }],
+      from: { type: 'phone', number: '919748849146' },
+      ncco,
+    }, (err, result) => {
+      res.json(err || result);
+    },
+  );
 });
 
 app
